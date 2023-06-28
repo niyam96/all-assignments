8611e8b9ac81f41e2a1cec0f19638e5bcc76f108
niyam96 <niyam.shah@protonmail.com>
2023-06-28 22:57:12 +0300

    Adding code for 02-course-app-easy

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -9,46 +9,138 @@ let COURSES = [];
 
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+
+  var username = req.body.username;
+  var password = req.body.password;
+  var newAdminUser = {
+    "username": username,
+    "password": password
+  }
+
+  if(ADMINS.find(user => user.username === username))
+    return res.status(400).send(`Admin user ${username} already exists`);
+
+  ADMINS.push(newAdminUser);
+
+  return res.status(200).send(`Admin ${username} created successfully`);
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+app.post('/admin/login', authenticateUser(ADMINS), (req, res) => {  
+  return res.status(200).send(`Logged in successfully`);
 });
 
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+app.post('/admin/courses', authenticateUser(ADMINS), (req, res) => {
+  var courseId = COURSES.length;
+  var courseDetails = {
+    "id": courseId,
+    "title": req.body.title,
+    "description": req.body.description,
+    "price": req.body.price,
+    "imageLink": req.body.imageLink,
+    "published": req.body.published
+  };
+
+  COURSES.push(courseDetails);
+
+  return res.status(200).send({"message": "Course created successfully", "courseId": courseId});
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+app.put('/admin/courses/:courseId', authenticateUser(ADMINS), (req, res) => {
+  var courseId = req.params.courseId;
+
+  if(courseId >= COURSES.length)
+    return res.status(400).send(`Course with ID : ${courseId} doesn't exist`);
+  
+  COURSES[courseId].title = req.body.title;
+  COURSES[courseId].description = req.body.description;
+  COURSES[courseId].imageLink = req.body.imageLink;
+  COURSES[courseId].price = req.body.price;
+  COURSES[courseId].published = req.body.published;
+
+  return res.status(200).send(`Course with ID : ${courseId} updated successfully`);
 });
 
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+app.get('/admin/courses', authenticateUser(ADMINS), (req, res) => {
+  var courseList = {
+    "courses": COURSES
+  }
+
+  return res.status(200).send(courseList);
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+  var username = req.body.username;
+  var password = req.body.password;
+  var newUser = {
+    "username": username,
+    "password": password,
+    "purchasedCourses": []
+  }
+
+  if(USERS.find(user => user.username === username))
+    return res.status(400).send(`User ${username} already exists`);
+
+  USERS.push(newUser);
+
+  return res.status(200).send(`User ${username} created successfully`);
 });
 
-app.post('/users/login', (req, res) => {
-  // logic to log in user
+app.post('/users/login', authenticateUser(USERS), (req, res) => {
+  return res.status(200).send(`Logged in successfully`);
 });
 
-app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+app.get('/users/courses', authenticateUser(USERS), (req, res) => {
+  var courseList = {
+    "courses": COURSES
+  }
+
+  return res.status(200).send(courseList);
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+app.post('/users/courses/:courseId', authenticateUser(USERS), (req, res) => {
+  var courseId = req.params.courseId;
+
+  if(courseId >= COURSES.length)
+    return res.status(400).send(`Course with ID : ${courseId} doesn't exist`);
+  
+  var user = USERS.find(({username}) => username === req.headers.username);
+  user.purchasedCourses.push(courseId);
+
+  return res.status(200).send(`Course with ID : ${courseId} purchased successfully`);
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+app.get('/users/purchasedCourses', authenticateUser(USERS), (req, res) => {
+  var user = USERS.find(({username}) => username === req.headers.username);
+
+  var purchasedCourses = []
+  user.purchasedCourses.forEach(courseId => {
+    purchasedCourses.push(COURSES[courseId]);
+  });
+
+  return res.status(200).send({
+    "purchasedCourses": purchasedCourses
+  })
 });
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
+
+function authenticateUser(userList)
+{
+  return (req, res, next) => {
+    var uname = req.headers.username;
+    var password = req.headers.password;
+
+    var user = userList.find(({username}) => username === uname);
+
+    if(!user)
+      return res.status(400).send(`Login Failed. User : ${uname} does not exist`);
+
+    if(user.password !== password)
+      return res.status(400).send(`Login failed for user : ${uname}`);
+
+    return next();
+  }
+}
